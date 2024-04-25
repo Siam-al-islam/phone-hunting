e7aabc38be8d3868041229493b1a1e6569a205df
Siam-al-islam <siamalislam692@gmail.com>
2024-04-25 22:07:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const loadPhoneData = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
+    const phones = data.data;
+    displayPhones(phones);
+}
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    const showAllBtn = document.getElementById('showAll');
+    if (phones.length > 12) {
+        showAllBtn.classList.remove('hidden')
+    };
+    phones = phones.slice(0, 11);
+    // console.log(phones.length)
+    phones.forEach(phone => {
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}"
+        alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhoneData(searchText);
+}
+
+loadPhoneData()
